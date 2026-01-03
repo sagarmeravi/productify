570609839ccdbe46f3d3854df714cd013b171e8a
sagarmeravi <sagarmeravi563@gmail.com>
2026-01-03 12:51:25 +0530

    Initial project setup

---
+++ backend/src/index.ts
@@ -0,0 +1,18 @@
+import express, { urlencoded } from "express";
+import { ENV } from "./config/env";
+import { clerkMiddleware } from '@clerk/express'
+import cors from "cors"
+const app = express();
+
+app.use(cors({origin:"http://localhost:5173"}))
+app.use(clerkMiddleware())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get("/",(req,res)=>{
+  res.send("Server!!")
+})
+
+app.listen(ENV.PORT, () => {
+  console.log("server is running",ENV.PORT);
+});
