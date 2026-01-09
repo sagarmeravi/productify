f9e45efe78f83afd5ca3a3e1727b4bcd58832eaf
sagarmeravi <sagarmeravi563@gmail.com>
2026-01-09 16:56:15 +0530

    Deployment changes added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,7 @@ import { User } from "./db/schema";
 import userRoutes from "./routes/userRoutes";
 import productRoutes from "./routes/productRoutes";
 import commentRoutes from "./routes/commentRoutes";
+import path from "path";
 
 const app = express();
 
@@ -14,13 +15,23 @@ app.use(clerkMiddleware());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get("/api/health", (req, res) => {
   res.send("Server!!");
 });
 
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/comments", commentRoutes);
+
+if (ENV.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 app.listen(ENV.PORT, () => {
   console.log("server is running", ENV.PORT);
